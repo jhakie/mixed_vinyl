d30bd93c500166ab7f988cc84bbd706cd14e922d
jhakie <jhakie@yahoo.com>
2023-08-22 23:06:42 +0000

    First controller and route.

---
+++ src/Controller/VinylController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controller;
+
+class VinylController
+{
+    #[Route('/')]
+    public function homepage()
+    {
+        die('Vinyl Def not fancy-looking frisbee!');
+    }
+}
